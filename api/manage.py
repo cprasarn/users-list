1d376cf93cd1c36eed99660027e62ddfcb86ccef
Noi <noi@localhost.localdomain>
2019-10-20 15:56:51 +0700

    Bugs fixed to support new Flask configuration

--- api/manage.py
+++ api/manage.py
@@ -1,9 +1,15 @@
+import os
 from flask import Flask
 from flask_script import Manager
 
 from app import create_app, db
 
-app = create_app('dev')
+if 'PRODUCTION' in os.environ and os.environ['PRODUCTION']:
+    app = create_app('production')
+elif 'TESTING' in os.environ and os.environ['TESTING']:
+    app = create_app('testing')
+else:
+    app = create_app('dev')
 
 manager = Manager(app)
 
